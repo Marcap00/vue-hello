fbe2ea31ded4e093eae1dfb187f53ff0ef5f3f5c
Marcap00 <cappellatomarco@yahoo.com>
2024-09-03 15:10:40 +0200

    refining

--- js/script.js
+++ js/script.js
@@ -4,8 +4,14 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            /* Title property */
             title: 'Il mio primo progetto con Vue',
-            url: 'imgs/code.jpg'
+            classTitle: 'text-center text-primary my-3',
+            /* Img container property */
+            classImgCont: 'd-flex justify-content-center',
+            /* Img property */
+            url: 'imgs/code.jpg',
+            classImg: 'border rounded-5'
         }
     }
 }).mount('#app')
