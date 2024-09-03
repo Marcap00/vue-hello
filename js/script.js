b3d9a7e198b5de639733a18446e0812a112d1d49
Marcap00 <cappellatomarco@yahoo.com>
2024-09-03 14:27:07 +0200

    exercise completed

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            title: 'Il mio primo progetto con Vue',
+            /* url: '' */
+        }
+    }
+}).mount('#app')
